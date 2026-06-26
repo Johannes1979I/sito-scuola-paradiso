0cfe0ef5b90ecc3f87170c11f60b7895a3a75aff
Gianpaolo Zarletti <gianpaolo.zarletti@gmail.com>
2026-06-26 16:35:05 +0200

    Quarantennale in cima alla home con effetti + splash 40 anni nell'intro prima del logo; logo Le Vie d'Europa; premiati cliccabili (testi); fix modale immagine modellino

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -535,8 +535,8 @@
             au.volume = 0.9;
             var p = au.play(); if (p && p.catch) { p.catch(function () {}); }
           }
-          setTimeout(fadeAudio, 2200);
-          setTimeout(function () { if (intro && intro.parentNode) { intro.remove(); } }, 4300);
+          setTimeout(fadeAudio, 3800);
+          setTimeout(function () { if (intro && intro.parentNode) { intro.remove(); } }, 5900);
           // salta-intro col clic (armato dopo 700ms per non scattare sul clic d'ingresso)
           setTimeout(function () {
             intro.addEventListener("click", function () {
